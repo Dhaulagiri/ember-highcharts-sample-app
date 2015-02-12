372f3bef683b3a0113eb7ccd7fef5d27806e40dd
Brian Runnells <brian.runnells@gmail.com>
2015-02-11 19:24:22 -0600

    Add initial chart example

---
+++ app/controllers/index.js
@@ -0,0 +1,29 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  options: {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+          text: 'Fruit eaten'
+      }
+    }
+  },
+  chartData: [
+    {
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }
+  ]
+});
