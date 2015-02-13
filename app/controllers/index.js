0ff700a6cf982541724be4bc3e29a4b4ee2c2b02
Brian Runnells <brian.runnells@gmail.com>
2015-02-13 07:58:11 -0600

    Add Pie Chart

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   options: {
     chart: {
-        type: 'bar'
+      type: 'bar'
     },
     title: {
       text: 'Fruit Consumption'
@@ -25,5 +25,49 @@ export default Ember.Controller.extend({
       name: 'John',
       data: [5, 7, 3]
     }
-  ]
+  ],
+  pieOptions: {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    title: {
+        text: 'Browser market shares at a specific website, 2014'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+            style: {
+                color: 'black'
+            },
+            connectorColor: 'silver'
+        }
+      }
+    }
+  },
+  pieChartData: [{
+      type: 'pie',
+      name: 'Browser share',
+      data: [
+        ['Firefox',   45.0],
+        ['IE',       26.8],
+        {
+            name: 'Chrome',
+            y: 12.8,
+            sliced: true,
+            selected: true
+        },
+        ['Safari',    8.5],
+        ['Opera',     6.2],
+        ['Others',   0.7]
+      ]
+  }]
 });
